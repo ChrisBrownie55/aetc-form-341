64cbfe0ce23d9cec1fee19154a56b67b6fb467db
Christopher Brown <chbphone55@gmail.com>
2019-10-30 09:41:18 -0500

    fix print on slower devices

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React, { useState, useCallback, useMemo } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, useCallback, useMemo } from "react";
+import ReactDOM from "react-dom";
 
-import './styles.css';
-import Form341 from './components/Form341';
-import { PAPER_341_WIDTH, PAPER_341_HEIGHT } from './constants';
+import "./styles.css";
+import Form341 from "./components/Form341";
+import { PAPER_341_WIDTH, PAPER_341_HEIGHT } from "./constants";
 
-function useInput({ initialValue = '', ...props }) {
+function useInput({ initialValue = "", ...props }) {
   const [value, setValue] = useState(initialValue);
   const handleChange = useCallback(event => setValue(event.target.value), [
     setValue
@@ -19,12 +19,11 @@ function useInput({ initialValue = '', ...props }) {
 }
 
 function printForm() {
-  const dataURL = document.querySelector('canvas').toDataURL('image/png');
+  const dataURL = document.querySelector("canvas").toDataURL("image/png");
   const windowHTML = `
     <!DOCTYPE html>
     <html lang="en">
       <head>
-        <title>AETC Form 341</title>
       </head>
       <body style="margin: 0;">
         <img src="${dataURL}" style="width: 100vh; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%) rotate(90deg);" />
@@ -33,24 +32,27 @@ function printForm() {
   `;
 
   const printWindow = window.open(
-    '',
-    '',
+    "",
+    "",
     `width=${PAPER_341_WIDTH},height=${PAPER_341_HEIGHT}`
   );
   printWindow.document.open();
   printWindow.document.write(windowHTML);
-  printWindow.document.close();
-  printWindow.focus();
-  printWindow.print();
+
+  setTimeout(() => {
+    printWindow.document.close();
+    printWindow.focus();
+    printWindow.print();
+  }, 100);
 }
 
 function App() {
-  const [name, setName, nameInput] = useInput({ placeholder: 'Smith John A' });
-  const [grade, setGrade, gradeInput] = useInput({ placeholder: 'E-4' });
+  const [name, setName, nameInput] = useInput({ placeholder: "Smith John A" });
+  const [grade, setGrade, gradeInput] = useInput({ placeholder: "E-4" });
   const [organization, setOrganization, organizationInput] = useInput({
-    placeholder: '123 TRS, Bldg 4567, Room D410'
+    placeholder: "123 TRS, Bldg 4567, Room D410"
   });
-  const [flight, setFlight, flightInput] = useInput({ placeholder: 'DOD' });
+  const [flight, setFlight, flightInput] = useInput({ placeholder: "DOD" });
 
   const formData = useMemo(() => ({ name, grade, organization, flight }), [
     name,
@@ -71,5 +73,5 @@ function App() {
   );
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
